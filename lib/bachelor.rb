7b8119fbd1a1d79eafea26460d06e56efb85ba77
Alex Bujenita <dekc89@gmail.com>
2018-12-18 21:46:38 +0000

    JSON retrieval DONE

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,42 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contestant|
+    return contestant["name"].split(" ")[0] if contestant["status"] == "Winner"
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      return contestant["name"] if contestant["occupation"] == occupation
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      if contestant["hometown"] == hometown
+        counter += 1
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      return contestant["occupation"] if contestant["hometown"] == hometown
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  total_age = 0
+  data[season].each do |contestant|
+    total_age += contestant["age"].to_f
+  end
+  puts data[season].length
+  (total_age/(data[season].length)).round
 end
